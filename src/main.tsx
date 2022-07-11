88fdfa3112d7c331ba57c0a3ef0355777270d3cc
guozh720 <1041674298@qq.com>
2022-07-12 00:18:55 +0800

    add home content

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import MyRouter from './router/index'
-import {MyRouter} from './router/route'
+import {MyRouter} from './router'
 import { BrowserRouter} from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 const Home = () => {
     return (
-        <div>
-            <h2>
-                这是主页
-            </h2>
+        <div style={
+            {
+                backgroundColor: 'rgb(4,7,32)',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }
+        }>
+            <p style={
+                {
+                   fontSize:'40px',
+                    /*设置线性渐变*/
+                   background: '-webkit-linear-gradient(left, #0f0, #00f) 0 0 no-repeat',
+                   WebkitBackgroundSize:'160px',
+                   WebkitBackgroundClip:'text',
+                   WebkitTextFillColor:' rgba(255, 255, 255, 0.3)',
+                }
+            }> coding or die</p>
         </div>
     )
 }
