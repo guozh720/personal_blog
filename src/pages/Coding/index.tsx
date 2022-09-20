24813fd353622d157ebe83ee5aa10107ec9a2b27
guozh720 <1041674298@qq.com>
2022-09-20 16:03:36 +0800

    test react-simplemde-editor for md plugins in react demo

--- src/pages/Coding/index.tsx
+++ src/pages/Coding/index.tsx
@@ -2,28 +2,33 @@ import React, {useEffect, useState} from "react";
 import {getCodingArticleApi, CodingArticle} from "../../mockService/MockCodingArticleService";
 import ReactMarkdown from 'react-markdown'
 import {CodeBlock} from "../../components/CodeBlock";
+import {SimplemdeEditor} from "../../components/SimplemdeEditor";
 const Coding = () => {
-    const [codingArticle, setCodingArticle] = useState<CodingArticle[]>([]);
-    const markDownInfo = '## hello'
-    const fnGeCodingArticle = async () => {
-        try {
-            const data = await getCodingArticleApi;
-            console.log('get CodingArticle success');
-            setCodingArticle(data);
-        } catch (e) {
-            console.error(e);
-        }
-    };
-    useEffect(() => {
-        fnGeCodingArticle().then(() => [])
-    }, [])
+    // const [codingArticle, setCodingArticle] = useState<CodingArticle[]>([]);
+    // const markDownInfo = '## hello'
+    // const fnGeCodingArticle = async () => {
+    //     try {
+    //         const data = await getCodingArticleApi;
+    //         console.log('get CodingArticle success');
+    //         setCodingArticle(data);
+    //     } catch (e) {
+    //         console.error(e);
+    //     }
+    // };
+    // useEffect(() => {
+    //     fnGeCodingArticle().then(() => [])
+    // }, [])
     return (
         <>
             这是coding界面
-            <ReactMarkdown children={markDownInfo}/>
-            {codingArticle.length > 0 ? codingArticle[0].title : ''}
-            <hr/>
-            <CodeBlock/>
+            {/*<ReactMarkdown children={markDownInfo}/>*/}
+            {/*{codingArticle.length > 0 ? codingArticle[0].title : ''}*/}
+            {/*<hr/>*/}
+            {/*<CodeBlock/>*/}
+            <div style={{width:'500px',height:'400px'}}>
+                <SimplemdeEditor/>
+            </div>
+            
         </>
     )
 }
